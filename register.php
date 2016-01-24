7f1fdaacc4e5b513f802c0757a56c97bb2817f1b
Misha <m.danilevskiy@gmail.com>
2016-01-24 19:20:03 +0200

    Merge branch 'new_files'
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ register.php
@@ -0,0 +1,75 @@
+<?php
+
+if (isset($_POST['data'])) {
+    $note = $_POST['data'];
+    $data = fopen("users.json", "a+");
+    $r=false;
+    while (!feof($data)) {
+        $user = fgets($data);
+        $user = json_decode($user,true);
+        if ($_POST['data']['email']==$user['email']){
+            echo "email already registered";
+            $r=true;
+            break;
+        }
+    }
+
+    if ($r!=true){
+        $note=json_encode($note);
+        fwrite($data, $note . PHP_EOL);
+        fclose($data);
+    }
+}
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Guestbook</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+          integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+          integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+            integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+            crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div class="container">
+
+    <?php require_once 'navbar.php' ?>
+
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            Registration
+        </div>
+        <div class="panel-body">
+            <form method="post">
+                <div class="form-group">
+                    <label>Email</label>
+                    <input class="form-control" name="data[email]"/>
+                </div>
+
+                <div class="form-group">
+                    <label>Password</label>
+                    <input class="form-control" type="password" name="data[password]"/>
+                </div>
+
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" name="form"/>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
+
